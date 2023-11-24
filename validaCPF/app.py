07df5053b8010e00411e0ef3b16b3f4a604be759
IzabelaXavier19 <izabelasilva07@hotmail.com>
2023-11-24 10:45:54 -0300

    integrando com o banco de dados

--- validaCPF/app.py
+++ validaCPF/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+import pymysql
+
+db = pymysql.connect(host="127.0.0.1",
+                     user="root",
+                     password="aluno",
+                     database="ValidaCPF")
 
 def calcula_cpf(cpf):
     cpf = ''.join(filter(str.isdigit, cpf))
@@ -18,16 +24,30 @@ def calcula_cpf(cpf):
     v1 = (v1 % 11) % 10
     v2 = (v2 % 11) % 10
     
+
     return cpf.endswith(str(v1) + str(v2))
  
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = db.cursor()
+    sql = "SELECT * FROM  parceiros;"
+    cursor.execute(sql)
+    parceiros = cursor.fetchall()
+    return render_template('index.html', parceiros=parceiros)
 
 @app.route('/validar_cpf', methods=['post'])
 def validar_cpf():
     cpf=request.form['cpf']
     if calcula_cpf(cpf):
+        nome = request.form['nome']
+        cursor = db.cursor()
+        #define a consulta SQL para inserir o cpf na tabela
+        sql = "INSERT INTO PARCEIROS (cpf, nome) values ('"
+        sql += cpf + "','" + nome + "');"
+        # executa a consulta SQL com o valor do cpf
+        cursor.execute(sql)
+        # confirma a inserção dos dados no banco de dados
+        db.commit() 
         return '<p>CPF válido e processado</p>' +\
                 '<p>Para inserir um novo parceiro, clique <a href="http://127.0.0.1:5000/">aqui!</a></p>'
     else:
